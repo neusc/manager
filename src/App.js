5c53435a01261aa169fa2555377e881d4e3d94e1
neusc <neusc829@163.com>
2018-04-15 10:44:50 +0800

    引入firebase用于登陆认证

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import { View, Text } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducers from './reducers'
+import firebase from 'firebase'
 
 class App extends Component {
+  componentWillMount () {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyAb587jWFHGKwRs5kmQeP-8GyYZmEE8nKg',
+      authDomain: 'manager-dd368.firebaseapp.com',
+      databaseURL: 'https://manager-dd368.firebaseio.com',
+      projectId: 'manager-dd368',
+      storageBucket: 'manager-dd368.appspot.com',
+      messagingSenderId: '1089868311224'
+    }
+    firebase.initializeApp(config)
+  }
+
   render () {
     return (
       <Provider store={createStore(reducers)}>
