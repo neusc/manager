4c8f671a1821209ecd4828a6a756fb474c58dde1
neusc <neusc829@163.com>
2018-04-19 15:57:22 +0800

    搭建登陆、雇员列表和雇员创建页面的路由结构

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Scene, Router } from 'react-native-router-flux'
+import { Scene, Router, Actions } from 'react-native-router-flux'
 import LoginForm from './components/LoginForm'
 import EmployeeList from './components/EmployeeList'
+import EmployeeCreate from './components/EmployeeCreate'
 
 const RouterComponent = () => {
   return (
@@ -11,7 +12,19 @@ const RouterComponent = () => {
           <Scene key="login" component={LoginForm} title="Please Login" initial />
         </Scene>
         <Scene key="main">
-          <Scene key="employeeList" component={EmployeeList} title="employee" />
+          <Scene
+            rightTitle="Add"
+            onRight={() => Actions.employeeCreate()}
+            key="employeeList"
+            component={EmployeeList}
+            title="employee"
+            initial
+          />
+          <Scene
+            key="employeeCreate"
+            component={EmployeeCreate}
+            title="Create Employee"
+          />
         </Scene>
       </Scene>
     </Router>
---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class EmployeeCreate extends Component {
+  render () {
+    return (
+      <View>
+        <Text>Employee Create</Text>
+      </View>
+    )
+  }
+}
+
+export default EmployeeCreate
