6d8b86d47a5906bf8f5ab91efe3be541e4f4b1ce
neusc <neusc829@163.com>
2018-04-23 14:53:48 +0800

    使用Cloud Firestore作为App数据库，并向数据库中添加数据结构和记录

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase'
+// Required for side-effects
+require('firebase/firestore')
+import { Actions } from 'react-native-router-flux'
 import { EMPLOYEE_UPDATE } from './types'
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -8,5 +12,13 @@ export const employeeUpdate = ({ prop, value }) => {
 }
 
 export const employeeCreate = ({ name, phone, shift }) => {
-  console.log(name, phone, shift)
+  let db = firebase.firestore()
+  let userId = firebase.auth().currentUser.uid
+  // 不需要返回action，只需要返回一个纯函数就行
+  // 此处使用firebase的Cloud Firestore作为数据库 https://firebase.google.com/docs/firestore/data-model
+  return () => {
+    db.collection('users').doc(userId).collection('employees').add({ name, phone, shift })
+      .then(() => Actions.employeeList())
+      .catch(error => {console.log(error)})
+  }
 }
