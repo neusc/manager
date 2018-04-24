1715098ffcf90b3ee89673af20d4b932c1c92617
neusc <neusc829@163.com>
2018-04-24 17:12:16 +0800

    使用react-native-communications库调用系统api实现通话、短信提醒等功能

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Communications from 'react-native-communications'
 import { Card, CardSection, Button } from './common'
 import EmployeeForm from './EmployeeForm'
 import { employeeUpdate, employeeSave } from '../actions'
@@ -18,13 +19,25 @@ class EmployeeEdit extends Component {
     this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid })
   }
 
+  onTextPress () {
+    const { phone, shift } = this.props
+    Communications.text(phone, `Your upcoming shift is on ${shift}`)
+  }
+
   render () {
     return (
       <Card>
         <EmployeeForm />
+
         <CardSection>
           <Button onPress={() => this.onButtonPress()}>
             Save Changes
+          </Button>
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.onTextPress()}>
+            Text Schedule
           </Button>
         </CardSection>
       </Card>
