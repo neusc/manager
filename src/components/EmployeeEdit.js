667ca95bf5f7a283e8f740b60552d79a0e7d1713
neusc <neusc829@163.com>
2018-04-24 15:08:39 +0800

    使用雇员列表页面点击的雇员信息prop填充reducer，避免直接修改prop传入的雇员信息造成脏数据污染

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,16 +1,29 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Card, CardSection, Button } from './common'
 import EmployeeForm from './EmployeeForm'
+import { employeeUpdate, employeeSave } from '../actions'
 
 class EmployeeEdit extends Component {
+  componentWillMount () {
+    // 使用雇员列表页面点击的雇员信息prop填充reducer
+    _.map(this.props.employee, (value, prop) => {
+      this.props.employeeUpdate({ prop, value })
+    })
+  }
+
+  onButtonPress () {
+    const { name, phone, shift } = this.props
+    console.log(name, phone, shift)
+  }
 
   render () {
     return (
       <Card>
         <EmployeeForm />
         <CardSection>
-          <Button>
+          <Button onPress={() => this.onButtonPress()}>
             Save Changes
           </Button>
         </CardSection>
@@ -19,4 +32,9 @@ class EmployeeEdit extends Component {
   }
 }
 
-export default connect()(EmployeeEdit)
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.employeeForm
+  return { name, phone, shift }
+}
+
+export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit)
